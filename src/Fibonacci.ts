24570424b30393793931987cb4bd6104136debb9
shiverzheng <zhengxiwen@funlearnworld.com>
2020-11-10 17:51:42 +0800

    a series questions of fibonacci

--- src/Fibonacci.ts
+++ src/Fibonacci.ts
@@ -30,3 +30,63 @@ class Fibonacci {
         return dp[n];
     }
 }
+
+/**
+ * 一直青蛙一次可以跳上1级台阶，也可以跳上2级台阶。求该青蛙跳上一个n级的台阶总共有多少种跳法（先后次序不同算不同的结果）。
+ *
+ * 思路:
+ *  跳三级台阶等于跳两级台阶的跳法+跳一级台阶的跳法。
+ *  跳四级台阶等于跳三级台阶+跳二级台阶的跳法。
+ *
+ *  符合斐波那契数列的规律
+ */
+
+function jumpFloor(n: number) {
+    if (n <= 2) return n;
+    return jumpFloor(n - 1) + jumpFloor(n - 2);
+}
+
+function jumpFloorDP(n: number) {
+    if (n <= 2) return n;
+    let result = 0;
+    let jump1 = 1;
+    let jump2 = 2;
+    for (let i = 3; i <= n; i++) {
+        result = jump1 + jump2;
+        jump1 = jump2;
+        jump2 = result;
+    }
+    return result;
+}
+
+/**
+ * 我们可以用2*1的小矩形横着或者竖着去覆盖更大的矩形。请问用n个2*1的小矩形无重叠的覆盖一个2*n的大矩形，总共有多少种方法？
+ *
+ * 例如 [# #]表示横向2*1
+ *      |#|
+ *      |#|表示竖向1*2
+ *
+ * 第n-2项 第n-1项   第n项
+ *  |#|    |#|#|️  |#|#|️#|
+ *  |#|    |#|#|  |#|#|#|
+ *
+ *         [# #]  [# #]|️#|
+ *         [# #]  [# #]|️#|
+ *
+ *                |️#|[# #]
+ *                |️#|[# #]
+ */
+
+function rectCover(n: number) {
+    if (n <= 2) return n;
+    let i = 2;
+    let pre = 1;
+    let current = 2;
+    let result = 0;
+    while (i++ < n) {
+        result = pre + current;
+        pre = current;
+        current = result;
+    }
+    return result;
+}
